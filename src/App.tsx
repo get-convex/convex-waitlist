5b66525008c1378a59b18bb5e98b3b18ade102d6
Michal Srb <srb@convex.dev>
2023-11-21 15:58:57 -0800

    Fix app and cleanup loadtest

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,6 @@ import { useEffect, useState } from "react";
 
 function App() {
   const sessionId = useSessionId();
-  const numbers = useQuery(api.myFunctions.listNumbers, {
-    count: 10,
-    sessionId,
-  });
-  const addNumber = useMutation(api.myFunctions.addNumber);
 
   return (
     <main className="container max-w-2xl flex flex-col gap-8">
@@ -36,28 +31,7 @@ function App() {
           </>
         )}
       >
-        <p>
-          Click the button and open this page in another window - this data is
-          persisted in the Convex cloud database!
-        </p>
-        <p>
-          <Button
-            onClick={() => {
-              void addNumber({
-                value: Math.floor(Math.random() * 10),
-                sessionId,
-              });
-            }}
-          >
-            Add a random number
-          </Button>
-        </p>
-        <p>
-          Numbers:{" "}
-          {numbers?.length === 0
-            ? "Click the button!"
-            : numbers?.join(", ") ?? "..."}
-        </p>
+        <NumbersDemo sessionId={sessionId} />
       </Waitlist>
       <p>
         Check out{" "}
@@ -73,6 +47,40 @@ function formatCardinal(x: number) {
   const suffix = ["th", "st", "nd", "rd"];
   const v = x % 100;
   return x + (suffix[(v - 20) % 10] ?? suffix[v] ?? suffix[0]);
+}
+
+function NumbersDemo({ sessionId }: { sessionId: string }) {
+  const numbers = useQuery(api.myFunctions.listNumbers, {
+    count: 10,
+    sessionId,
+  });
+  const addNumber = useMutation(api.myFunctions.addNumber);
+  return (
+    <>
+      <p>
+        Click the button and open this page in another window - this data is
+        persisted in the Convex cloud database!
+      </p>
+      <p>
+        <Button
+          onClick={() => {
+            void addNumber({
+              value: Math.floor(Math.random() * 10),
+              sessionId,
+            });
+          }}
+        >
+          Add a random number
+        </Button>
+      </p>
+      <p>
+        Numbers:{" "}
+        {numbers?.length === 0
+          ? "Click the button!"
+          : numbers?.join(", ") ?? "..."}
+      </p>
+    </>
+  );
 }
 
 const STORE = (typeof window === "undefined" ? null : window)?.sessionStorage;
